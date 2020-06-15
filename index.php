8b09e80e2d777f84a806d16d25f2e0b6b2d9587f
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-15 15:18:09 -0700

    add permalinks to blog posts

--- index.php
+++ index.php
@@ -15,13 +15,22 @@ The main template file
 <!-- make blog posts dynamic instead of hard coding the html -->
       <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
         <div>
+          <!-- read more link in title-->
+          <a href="<?php the_permalink(); ?>">
           <h2><?php the_title(); ?></h2>
+          </a>
           <p><?php echo get_the_date('F j, Y'); ?> by <a href=""><?php the_author();?></a></p>
           <div class="pb-2">
             <i class="fas fa-tags"></i>
             <p class="d-inline"><?php the_tags('Tagged: ', ' ~ ')?></p>
           </div>
           <p><?php the_excerpt() ?></p>
+          <!--read more link -->
+          <div class="mb-3">
+            <a href="<?php the_permalink(); ?>">
+              <?php _e('Read more...') ?>
+            </a>
+          </div>
         </div>
         <?php endwhile; else :  ?>
 
