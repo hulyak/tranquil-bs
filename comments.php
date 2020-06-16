33730ef7410779cbc6ba4e5246989a5d1baf2c18
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-15 18:55:11 -0700

    show person's avatar when they comment on blog

--- comments.php
+++ comments.php
@@ -8,6 +8,54 @@ https://developer.wordpress.org/reference/functions/comment_form/-->
 <!-- => assigns to new value 
 add bootstrap classes -->
 <?php 
+// if the post is password protected, return without loading up any comments
+if(post_password_required()){
+  return;
+}
+?>
+
+<section>
+  <?php if(have_comments() ): ?>
+    <h2 class="py-3">
+      <?php
+       $number_of_comments = get_comments_number();
+       if( $number_of_comments === 1 ) {
+        printf(
+          // %s make it plural ldquo -left double quotation 
+          _x(
+            'One comment on &ldquo;%s&rdquo;',
+            'comments title'
+          ),
+          get_the_title()
+        );
+       } else {
+        printf(
+          _nx(
+            '%1$s comment on &ldquo;%2$s&rdquo;',
+            '%1$s comments on &ldquo;%2$s&rdquo;',
+            $number_of_comments,
+            'comments title'
+          ),
+          number_format_i18n($number_of_comments),
+          get_the_title()
+        );
+       }
+      ?>
+    </h2>
+
+    <ol>
+      <?php 
+      wp_list_comments(array(
+        'style' => 'ol',
+        'avatar_size' => 70
+      ))
+      ?>
+    </ol>
+  <?php endif; ?>
+</section>
+
+
+<?php 
 $args = array(
   'title_reply' => '<div class="py-3">Reply or Comment</div>',
 
